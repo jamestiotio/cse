07f7b9e516ef1ac7b32608f77215420b108ece52
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-03-10 09:09:03 +0800

    IT WORKS ㅠㅠㅠ

--- pa/PA1/shellPrograms/shellDaemonize_code.c
+++ pa/PA1/shellPrograms/shellDaemonize_code.c
@@ -13,8 +13,8 @@
 #include "shellPrograms.h"
 
 // TODO: change to appropriate path
-char *path = "/Users/natalie_agus/Dropbox/50.005 Computer System Engineering/2020/PA1 Makeshell Daemon/PA1/logfile_test.txt";
-//char *path = "/Users/caramelmelmel"
+//char *path = "/Users/natalie_agus/Dropbox/50.005 Computer System Engineering/2020/PA1 Makeshell Daemon/PA1/logfile_test.txt";
+char *path = "/Users/caramelmel/cse/pa/PA1/systemlog.txt";
 
 /*This function summons a daemon process out of the current process*/
 static int create_daemon()
@@ -26,11 +26,10 @@ static int create_daemon()
     // 2. Close parent with exit(1)
     // 3. On child process (this is intermediate process), call setsid() so that the child becomes session leader to lose the controlling TTY
     // 4. Ignore SIGCHLD, SIGHUP
-
     // 5. Fork() again, parent (the intermediate) process terminates
-
     // 6. Child process (the daemon) set new file permissions using umask(0). Daemon's PPID at this point is 1 (the init)
     // 7. Change working directory to root
+
     // 8. Close all open file descriptors using sysconf(_SC_OPEN_MAX) and redirect fd 0,1,2 to /dev/null
     // 9. Return to main
     pid_t pid = fork();
@@ -45,21 +44,50 @@ static int create_daemon()
         signal(SIGHUP, SIG_IGN);
 
         //TODO 5 
-        
+        pid_t pid_2 = fork();
+
+        if(pid_2< 0 ){
+            fprintf(stderr,"Fork has failed exiting now\n");
+            return 1;
+        }
+        else if(pid_2 > 0){
+            printf("Closing the parent process right now\n");
+            exit(1);
+        }
+        else if (pid_2 == 0){
+            //child process daemon
+            //set file mode and create task
+            //DEBUGGING NEEDED: the error on the compiler 
+            umask(0);
+
+            //THIS IS TO TEST FOR DEBUGGING AS UNMASK THREW AN ERROR ON MY COMPILER
+            //printf("compiled here\n");
+
+            //TASK 7
+            chdir("/");
+            
+            //TASK 8 
+            //loop through the filelines 
+            for(int i = sysconf(_SC_OPEN_MAX);i>=0;i--){
+                close(i);
+            }
+            int fd0 = open("/dev/null", O_RDWR);
+            int fd1 = dup(0);
+            int fd2 = dup(0);
+        }
 
     }
     else if(pid > 0){
         //TO INDICATE TO YOU this is part 2
-        printf("closing the parent process right now");
+        printf("closing the parent process right now\n");
         exit(1);
     }
 
     else{
-        fprintf(stderr,"Fork has failed, exiting now");
+        fprintf(stderr,"Fork has failed, exiting now\n");
         //exit with error
         return 1;
     }
-
     return 1;
 }
 
@@ -104,10 +132,12 @@ static int daemon_work()
 
     return EXIT_SUCCESS;
 }
+
 int main(int argc, char **args)
 {
     create_daemon();
 
+    printf("created daemon correctly\n");
     /* Open the log file */
     openlog("customdaemon", LOG_PID, LOG_DAEMON);
     syslog(LOG_NOTICE, "Daemon started.");
---
+++ pa/PA1/testcases/check_compshelldaemon.c
@@ -0,0 +1,27 @@
+//THIS FILE IS TO TEST UNMASK AS IT WAS not working on my system 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
+#include <dirent.h>
+#include <errno.h>
+/* "readdir" etc. are defined here. */
+#include <dirent.h>
+/* limits.h defines "PATH_MAX". */
+#include <limits.h>
+#include <ctype.h>
+
+#include <signal.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+
+int main(){
+    unmask(0);
+    printf("Managed to set file permissions to 0777");
+    return 1;
+}
